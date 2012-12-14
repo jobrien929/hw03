71bd647e6bfadd1d904ccfe775e8c638f0e23c5b
zoukai957 <songios2003@yahoo.com>
2012-12-13 22:37:59 -0500

    Hey guys! Just finished the function you guys requested from me (at least to how I understand how our code works)and it has been 100% tested. Again I have modified the other files so PLEASE read the group5.odt to see what changes Ive made in case it effects your code. You shouldn't need to link anything to make this code work... if you need anything else from me hit me up

---
+++ linkstateManage.h
@@ -0,0 +1,7 @@
+#include "uthash.h"
+#include "cs352proxy.h"
+
+/**removeExpired uses the global variable "linkTimeOUt" to find all 
+ *    expired linksStates in the "node_list" and removes them
+ * returns nothing for now**/
+void removeExpired();
